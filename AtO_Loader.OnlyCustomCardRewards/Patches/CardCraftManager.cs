4c3e466068a8a687729e55b5945af77d2b78e460
IcyPhoenix <2542611+PhoenixICE@users.noreply.github.com>
2022-11-03 15:33:10 +1100

    Also patch shop

--- AtO_Loader.OnlyCustomCardRewards/Patches/CardCraftManager.cs
+++ AtO_Loader.OnlyCustomCardRewards/Patches/CardCraftManager.cs
@@ -6,22 +6,27 @@ using static Enums;
 
 namespace AtO_Loader.OnlyCustomCardRewards.Patches;
 
-[HarmonyPatch(typeof(RewardsManager),  "SetRewards")]
-public class ShowListCardsForCraft
+[HarmonyPatch(typeof(CardCraftManager), "ShowListCardsForCraft")]
+public static class CardCraftManager
 {
     private static Dictionary<CardClass, List<string>> cardListNotUpgradedByClass;
+    private static Dictionary<CardClass, List<string>> cardListByClass;
 
     [HarmonyPrefix]
     public static void OverwriteCardPool()
     {
+        cardListByClass = Globals.Instance.CardListByClass;
         cardListNotUpgradedByClass = Globals.Instance.CardListNotUpgradedByClass;
-        Globals.Instance.CardListNotUpgradedByClass = CreateCardClonesPrefix.CustomCards.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Id).ToList());
+        var tempDict = CreateCardClonesPrefix.CustomCards.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Id).ToList());
+        Globals.Instance.CardListByClass = tempDict;
+        Globals.Instance.CardListNotUpgradedByClass = tempDict;
     }
 
     [HarmonyPostfix]
     public static void RevertCardPool()
     {
         Globals.Instance.CardListNotUpgradedByClass = cardListNotUpgradedByClass;
+        Globals.Instance.CardListByClass = cardListByClass;
     }
 }
 
--- AtO_Loader.OnlyCustomCardRewards/Plugin.cs
+++ AtO_Loader.OnlyCustomCardRewards/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using AtO_Loader.Patches;
-using AtO_Loader.Patches.CustomDataLoader;
+﻿using AtO_Loader.OnlyCustomCardRewards.Patches;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -11,12 +10,13 @@ public partial class Plugin : BaseUnityPlugin
 {
     private const string modGUID = "IcyPhoenix.OnlyCustomCardRewards";
     private const string modName = "AtO_Loader.OnlyCustomCardRewards";
-    private const string ModVersion = "0.0.0.1";
+    private const string ModVersion = "0.0.1.0";
     private readonly Harmony harmony = new(modGUID);
     internal static new ManualLogSource Logger;
 
     private void Awake()
     {
         Plugin.Logger = base.Logger;
+        this.harmony.PatchAll(typeof(ShowListCardsForCraft));
     }
 }
