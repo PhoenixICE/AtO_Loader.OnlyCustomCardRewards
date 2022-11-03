ee9a5fe948ec9393716167475d7de34e7eb35d93
IcyPhoenix <2542611+PhoenixICE@users.noreply.github.com>
2022-11-03 13:57:17 +1100

    Add project files.

---
+++ AtO_Loader.OnlyCustomCardRewards/Patches/ShowListCardsForCraft.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AtO_Loader.OnlyCustomCardRewards.Patches;
+
+public class ShowListCardsForCraft
+{
+
+}
+
---
+++ AtO_Loader.OnlyCustomCardRewards/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using AtO_Loader.Patches;
+using AtO_Loader.Patches.CustomDataLoader;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AtO_Loader.OnlyCustomCardRewards;
+
+[BepInPlugin(modGUID, modName, ModVersion)]
+public partial class Plugin : BaseUnityPlugin
+{
+    private const string modGUID = "IcyPhoenix.OnlyCustomCardRewards";
+    private const string modName = "AtO_Loader.OnlyCustomCardRewards";
+    private const string ModVersion = "0.0.0.1";
+    private readonly Harmony harmony = new(modGUID);
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Plugin.Logger = base.Logger;
+    }
+}
